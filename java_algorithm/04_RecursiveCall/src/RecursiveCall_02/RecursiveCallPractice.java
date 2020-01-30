940f8be3c8bc04113c5d6e258eccdde7c44f3f90
LeeJuneHyun <dlwnsgus777@naver.com>
2020-01-30 22:56:33 +0900

    2020-01-30

---
+++ java_algorithm/04_RecursiveCall/src/RecursiveCall_02/RecursiveCallPractice.java
@@ -0,0 +1,10 @@
+package RecursiveCall_02;
+
+public class RecursiveCallPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
