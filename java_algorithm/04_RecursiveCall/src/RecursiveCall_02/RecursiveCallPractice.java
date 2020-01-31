535f383580052b4603f3548f3436f500f2a87288
LeeJuneHyun <dlwnsgus777@naver.com>
2020-01-31 22:52:29 +0900

    2020-01-31

--- java_algorithm/04_RecursiveCall/src/RecursiveCall_02/RecursiveCallPractice.java
+++ java_algorithm/04_RecursiveCall/src/RecursiveCall_02/RecursiveCallPractice.java
@@ -1,10 +1,47 @@
 package RecursiveCall_02;
 
+import java.util.Arrays;
+
 public class RecursiveCallPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		int multiple = multiple(5);
+		System.out.println("multiple number = " + multiple);
+		int[] intArr = {1, 2, 3, 4, 5};
+		int sumNumber = sumNumber(intArr);
+		System.out.println("sum number = " + sumNumber);
+		
+		System.out.println("tomato는 회문인가?  " + palindrome("tomato"));
+		System.out.println("level는 회문인가?  " + palindrome("level"));
 	}
-
+	
+	public static int multiple(int num) {
+		if (num <= 1) {
+			return num;
+		} else {
+			return num * multiple(num - 1);
+		}
+	}
+	
+	public static int sumNumber(int[] num) {
+		if (num.length <= 1) {
+			return num[0];
+		} else {
+			return num[0] + sumNumber(Arrays.copyOf(num, num.length - 1));
+		}
+		
+	}
+	
+	public static Boolean palindrome(String str) {
+		if (str.length() <= 1) {
+			return true;
+		} 
+		
+		if (str.charAt(0) == str.charAt(str.length() - 1)) {
+			return palindrome(str.substring(1, str.length() - 1));
+		} else {
+			return false;
+		}
+	}
 }
