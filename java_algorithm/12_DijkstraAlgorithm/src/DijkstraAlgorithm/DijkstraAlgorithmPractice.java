e3d4f4a7176ab5b72b0f16ab67757a21b2996b43
LeeJuneHyun <dlwnsgus777@naver.com>
2020-02-06 21:34:05 +0900

    2020-02-06

---
+++ java_algorithm/12_DijkstraAlgorithm/src/DijkstraAlgorithm/DijkstraAlgorithmPractice.java
@@ -0,0 +1,17 @@
+package DijkstraAlgorithm;
+
+import java.util.HashMap;
+
+public class DijkstraAlgorithmPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// graph ±¸Çö
+		HashMap<String, HashMap<String, Integer>> myGraph = new HashMap<String, HashMap<String,Integer>>();
+
+		
+		System.out.println("test");
+	}
+
+}
